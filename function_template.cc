4039a9fbf5e133b69c07dd45bd5c2009e2b645b9
Peter Strøiman <peter@stroiman.com>
2024-11-24 11:08:48 +0100

    Implement very basic indexed property handler
    This only supports a "Getter" at the moment.

--- function_template.cc
+++ function_template.cc
@@ -38,8 +38,8 @@ void FunctionTemplateCallback(const FunctionCallbackInfo<Value>& info) {
     thisAndArgs[i] = tracked_value(ctx, val);
   }
 
-  goFunctionCallback_return retval =
-      goFunctionCallback(ctx_ref, callback_ref, thisAndArgs.data(), thisAndArgs.size() - 1);
+  goFunctionCallback_return retval = goFunctionCallback(
+      ctx_ref, callback_ref, thisAndArgs.data(), thisAndArgs.size() - 1, 0);
   if (retval.r1 != nullptr) {
     iso->ThrowException(retval.r1->ptr.Get(iso));
   } else if (retval.r0 != nullptr) {
